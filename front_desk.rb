da8462522a01de0ab038243519408425a613ecfb
Vicky Jackson <vicky@digitalstuff.net>
2018-07-08 23:11:18 +0100

    Added a front-end and fixed bugs around returning errors

--- front_desk.rb
+++ front_desk.rb
@@ -5,7 +5,7 @@ require_relative('guest')
 class FrontDesk
 
 # Getters
-  attr_reader :checked_in_guests, :till
+  attr_reader :rooms, :checked_in_guests, :till
 
 # Initialise argument list
   def initialize(rooms)
@@ -53,7 +53,7 @@ class FrontDesk
   # Add booking fee to till
   # Provide confirmation of reservation
   def create_reservation(guest, room, party_size)
-    if !is_room_available?(room, party_size)
+    if (!is_room_available?(room, party_size)) || (!guest.can_afford_to_pay?(@booking_fee))
       return nil
     end
     guest.pay(@booking_fee)
@@ -67,14 +67,44 @@ class FrontDesk
   # Add the reservation to the checked_in_guests array
   def check_in(guest)
     found_reservation = find_reservation_by_name(guest.name)
+    if found_reservation == nil
+      return "Sorry, we couldn't find your reservation."
+    end
     @checked_in_guests << found_reservation
+    return "Thanks, you are now checked in under #{guest.name}!"
+  end
+
+  def check_in_by_name(guest_name)
+    found_reservation = find_reservation_by_name(guest_name)
+    if found_reservation == nil
+      return "Sorry, we couldn't find your reservation."
+    end
+    @checked_in_guests << found_reservation
+    return "Thanks, you are now checked in under #{guest_name}!"
   end
 
   # Find the reservation and check the guest out
   # Remove the reservation from the checked_in_guests array
   def check_out(guest)
     found_reservation = find_reservation_by_name(guest.name)
-    @checked_in_guests.delete(found_reservation)
+    found_guest = find_checked_in_guest_by_name(guest.name)
+    if found_guest == nil
+      return "Sorry, we couldn't find your reservation."
+    end
+    @reservations.delete(found_reservation)
+    @checked_in_guests.delete(found_guest)
+    return "You are now checked out! See you next time!"
+  end
+
+  def check_out_by_name(guest_name)
+    found_reservation = find_reservation_by_name(guest_name)
+    found_guest = find_checked_in_guest_by_name(guest_name)
+    if found_guest == nil
+      return "Sorry, we couldn't find your reservation."
+    end
+    @reservations.delete(found_reservation)
+    @checked_in_guests.delete(found_guest)
+    return "You are now checked out! See you next time!"
   end
 
   # Find the room that the guest is in
---
+++ viewer.rb
@@ -0,0 +1,126 @@
+require_relative('front_desk')
+
+@room1 = Room.new("Swingin' 60s", 100, ["Good Vibrations","Mr Tabourine Man","Happy Together"])
+@room2 = Room.new("Groovy 70s", 80, ["Stayin' Alive","Dancing Queen","Night Fever"])
+@room3 = Room.new("Mad 90s", 50, ["Rhythm is a Dancer","Still Dre","Barbie Girl"])
+@front_desk = FrontDesk.new([@room1, @room2, @room3])
+
+# Methods
+
+def run_main_menu()
+  choice = ""
+  while choice != "5"
+    if choice == ""
+      puts "\e[H\e[2J"
+      puts "Hi, I'm KaraokeBot! Please select 1, 2, 3 or 4!"
+    else
+      puts "Thanks for using KaraokeBot! Can I help with anything else?"
+    end
+    puts ""
+    puts "1. Check if a particular room is available"
+    puts "2. Make a reservation"
+    puts "3. Check in"
+    puts "4. Check out"
+    puts "5. Exit"
+    puts ""
+    choice = gets.chomp
+    case choice
+      when "1"
+        is_room_available()
+      when "2"
+        make_reservation()
+      when "3"
+        check_in()
+      when "4"
+        check_out()
+    end
+  end
+end
+
+def get_room()
+  puts "These are our rooms - please select one to see if it's available!"
+  puts "1. #{@front_desk.rooms[0].name}"
+  puts "2. #{@front_desk.rooms[1].name}"
+  puts "3. #{@front_desk.rooms[2].name}"
+  room_choice = gets.chomp
+  case room_choice
+    when "1"
+      room = @front_desk.rooms[0]
+    when "2"
+      room = @front_desk.rooms[1]
+    when "3"
+      room = @front_desk.rooms[2]
+  end
+  return room
+end
+
+def get_party_size()
+  puts "How big is your party?"
+  party_size = gets.chomp.to_i
+end
+
+def is_room_available()
+  puts "\e[H\e[2J"
+  puts "You chose to check if a particular room is available"
+  party_size = get_party_size()
+  room = get_room()
+  if (@front_desk.is_room_available?(room, party_size)) == true
+    puts "\e[H\e[2J"
+    puts "#{room.name} is available for your party size!"
+  else
+    puts "#{room.name} is not available for a party that big!"
+  end
+end
+
+def get_guest()
+  puts "Please enter your name"
+  name = gets.chomp.to_s.capitalize
+  puts "Please tell me how much money is in your wallet"
+  wallet = gets.chomp.to_i
+  puts "Please tell me your favourite song"
+  favourite_song = gets.chomp.to_s.capitalize
+  return Guest.new(name, wallet, favourite_song)
+end
+
+def get_guest_name()
+  puts "Please enter your name"
+  name = gets.chomp.to_s.capitalize
+  return name
+end
+
+def make_reservation()
+  puts "\e[H\e[2J"
+  puts "You chose to make a reservation"
+  guest = get_guest()
+  room = get_room()
+  party_size = get_party_size()
+  reservation = @front_desk.create_reservation(guest, room, party_size)
+  if reservation == nil
+    puts "Sorry, we were unable to make the reservation."
+    puts "Maybe you can't or afford it, or the room isn't available."
+    puts ""
+  else
+    puts reservation
+    puts ""
+  end
+end
+
+def check_in()
+  puts "\e[H\e[2J"
+  puts "You chose to check in"
+  guest_name = get_guest_name()
+  puts check_in
+  puts ""
+end
+
+def check_out()
+  puts "\e[H\e[2J"
+  puts "You chose to check out"
+  guest_name = get_guest_name()
+  check_out = @front_desk.check_out_by_name(guest_name)
+  puts check_out
+  puts ""
+end
+
+# Everything else
+run_main_menu()
